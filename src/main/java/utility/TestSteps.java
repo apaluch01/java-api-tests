7c46e6ba97ed70fccb80adf37ea5ffc89ae077dd
apaluch <apaluch@griddynamics.com>
2022-08-22 14:33:49 +0200

    deleted unused methods

--- src/main/java/utility/TestSteps.java
+++ src/main/java/utility/TestSteps.java
@@ -4,27 +4,5 @@ import io.restassured.response.Response;
 
 public class TestSteps {
 
-    public void buildRequest() {
-
-    }
-
-    public void sendRequest() {
-
-    }
-
-    public boolean checkResponseIsValid(Response response) {
-        return(response.getStatusCode() == 200);
-    }
-
-    public int prepareActualResponse(Response response) {
-        return(response.getStatusCode());
-    }
-
-    public int prepareExpectedResponse(int statusCode) {
-        return(statusCode);
-    }
-
-    public boolean checkActualVsExpectedResponses(Response response, int statusCode) {
-        return(prepareActualResponse(response) == prepareExpectedResponse(statusCode));
-    }
+    //TODO buildRequest, sendRequest
 }
