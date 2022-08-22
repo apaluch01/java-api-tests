478b03660ec7fec75b678a8672a931311cfc7ba9
apaluch <apaluch@griddynamics.com>
2022-08-22 12:10:46 +0200

    added needed data as constants

--- src/main/java/utility/TestConfigurationData.java
+++ src/main/java/utility/TestConfigurationData.java
@@ -1,4 +1,11 @@
 package utility;
 
-public class TestConfigurationData {
+import java.util.HashMap;
+
+public interface TestConfigurationData {
+    public static final String BASE_URI = "https://api.trello.com";
+
+    public static final String KEY = "261478d38cec68e8e339c84b6096692d";
+
+    public static final String TOKEN = "10f8cec355295b29a25d24b2ae6a82b0fdda92c603bab4b2edda2fca4e88f4ed";
 }
