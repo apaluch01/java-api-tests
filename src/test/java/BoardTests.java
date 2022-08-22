0b8d9db0c2b81aac4e619a97f317d6dff43c0833
apaluch <apaluch@griddynamics.com>
2022-08-22 13:25:50 +0200

    adjusted responses and console output

--- src/test/java/BoardTests.java
+++ src/test/java/BoardTests.java
@@ -20,39 +20,37 @@ public class BoardTests extends BaseTest {
     void shouldCreateBoard() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
 
-        Response responsePost = requestSpecification.when().post("/1/boards/");
+        Response response = requestSpecification.when().post("/1/boards/");
 
         id = given().baseUri(BASE_URI).
                 queryParam("name", "test1").queryParam("key", KEY).queryParam("token", TOKEN).
                 contentType(ContentType.JSON).log().all().post("/1/boards/").
                 then().extract().path("id");
-        //System.out.println(response1.then().extract().response().asString());
-        //Assert.assertEquals(response1.statusCode(), 200);
     }
 
     @Test (priority = 2)
     void shouldGetBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response responseGet = requestSpecification.when().get("/1/boards/" + id);
+        Response response = requestSpecification.when().get("/1/boards/" + id);
 
-        System.out.println(responseGet.then().extract().response().asString());
-        Assert.assertEquals(responseGet.statusCode(), 200);
+        System.out.println(response.then().extract().response().asString());
+        Assert.assertTrue(checkResponseIsValid(response));
     }
 
     @Test (priority = 2)
     void shouldUpdateBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response responseGet = requestSpecification.when().put("/1/boards/" + id);
+        Response response = requestSpecification.when().put("/1/boards/" + id);
 
-        System.out.println(responseGet.then().extract().response().asString());
-        Assert.assertEquals(responseGet.statusCode(), 200);
+        System.out.println(response.then().extract().response().asString());
+        Assert.assertTrue(checkResponseIsValid(response));
     }
 
     @Test (priority = 3)
     void shouldDeleteBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response responseGet = requestSpecification.when().delete("/1/boards/" + id);
+        Response response = requestSpecification.when().delete("/1/boards/" + id);
 
-        Assert.assertEquals(responseGet.statusCode(), 200);
+        Assert.assertTrue(checkResponseIsValid(response));
     }
 }
