6bb9c266c469ca0e55003dff728dee1fea6d59b2
apaluch <apaluch@griddynamics.com>
2022-08-22 02:20:00 +0200

    created TestSteps class file with methods

--- src/main/java/utility/TestSteps.java
+++ src/main/java/utility/TestSteps.java
@@ -1,4 +1,27 @@
 package utility;
 
 public class TestSteps {
+    public void buildRequest() {
+
+    }
+
+    public void sendRequest() {
+
+    }
+
+    public void checkResponseIsValid() {
+
+    }
+
+    public void prepareActualResponse() {
+
+    }
+
+    public void prepareExpectedResponse() {
+
+    }
+
+    public void checkActualVsExpectedResponses() {
+
+    }
 }
