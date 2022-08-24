029a13a4ff90ee1c8f82b91ca07a64b8b6b9ae2d
apaluch <apaluch@griddynamics.com>
2022-08-25 01:20:27 +0200

    swapped constants to config file path

--- src/main/java/utility/TestConfigurationData.java
+++ src/main/java/utility/TestConfigurationData.java
@@ -1,9 +1,5 @@
 package utility;
 
 public class TestConfigurationData {
-    public static final String BASE_URI = "https://api.trello.com";
-
-    public static final String KEY = "261478d38cec68e8e339c84b6096692d";
-
-    public static final String TOKEN = "10f8cec355295b29a25d24b2ae6a82b0fdda92c603bab4b2edda2fca4e88f4ed";
+    public static final String CONFIG_DATA_FILE_PATH = "src/config.txt";
 }
