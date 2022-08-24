c20968c40b5abde6579926091d8240260730aea3
apaluch <apaluch@griddynamics.com>
2022-08-25 01:24:22 +0200

    cleared unused imports

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,9 @@
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import utility.TestSteps;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
