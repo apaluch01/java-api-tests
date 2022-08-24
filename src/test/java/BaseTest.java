82df1a96e829850747affe661ae9133cf46bf339
apaluch <apaluch@griddynamics.com>
2022-08-25 01:21:41 +0200

    builder changes for config file reading

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import utility.TestSteps;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static utility.TestConfigurationData.*;
 
 public abstract class BaseTest extends TestSteps {
@@ -17,9 +22,14 @@ public abstract class BaseTest extends TestSteps {
     public void setupRequestSpecBuilder()
     {
         builder = new RequestSpecBuilder();
-        builder.setBaseUri(BASE_URI);
-        builder.addQueryParam("key", KEY);
-        builder.addQueryParam("token", TOKEN);
+        try (FileReader file = new FileReader(CONFIG_DATA_FILE_PATH);
+             BufferedReader reader = new BufferedReader(file)) {
+            builder.setBaseUri(reader.readLine());
+            builder.addQueryParam("key", reader.readLine());
+            builder.addQueryParam("token", reader.readLine());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
         requestSpec = builder.build();
     }
 }
