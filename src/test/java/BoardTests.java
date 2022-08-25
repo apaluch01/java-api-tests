639e0bb16202beb797d230e89602912349cb4840
apaluch <apaluch@griddynamics.com>
2022-08-25 09:02:42 +0200

    added ids list and logger

--- src/test/java/BoardTests.java
+++ src/test/java/BoardTests.java
@@ -2,51 +2,60 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 import static io.restassured.RestAssured.given;
-import static utility.TestConfigurationData.*;
 
 public class BoardTests extends BaseTest {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(BoardTests.class));
+    public static final List<String> ids = new ArrayList();
 
-    String id = "";
+    @AfterSuite
+    public void cleanUpIds(){
+        ids.clear();
+    }
+
     @Test (priority = 1)
     void shouldCreateBoard() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
 
-        Response response = requestSpecification.queryParam("name", "TEST").contentType(ContentType.JSON).
+        Response response = requestSpecification.queryParam("name", "TEST").
+                contentType(ContentType.JSON).
                 when().post("/1/boards/");
 
-        id = given().
-                queryParam("name", "test1").spec(requestSpec).
-                contentType(ContentType.JSON).log().all().post("/1/boards/").
-                then().extract().path("id");
+        ids.add(response.then().extract().path("id"));
 
+        LOGGER.info(ids.get(0));
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test (priority = 2)
     void shouldGetBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response response = requestSpecification.when().get("/1/boards/" + id);
+        Response response = requestSpecification.when().get("/1/boards/" + ids.get(0));
 
-        System.out.println(response.then().extract().response().asString());
+        LOGGER.info(response.then().extract().response().asString());
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test (priority = 2)
     void shouldUpdateBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response response = requestSpecification.when().put("/1/boards/" + id);
+        Response response = requestSpecification.when().put("/1/boards/" + ids.get(0));
 
-        System.out.println(response.then().extract().response().asString());
+        LOGGER.info(response.then().extract().response().asString());
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test (priority = 3)
     void shouldDeleteBoardById() {
         RequestSpecification requestSpecification = given().spec(requestSpec);
-        Response response = requestSpecification.when().delete("/1/boards/" + id);
+        Response response = requestSpecification.when().delete("/1/boards/" + ids.get(0));
 
         Assert.assertEquals(response.getStatusCode(), 200);
     }
