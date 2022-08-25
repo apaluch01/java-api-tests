a3b61ba2c22d1dd3f37ffbfe6eb306fbdf8f9af8
apaluch <apaluch@griddynamics.com>
2022-08-25 09:51:58 +0200

    added createBoard method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,17 @@
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import utility.TestSteps;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static io.restassured.RestAssured.given;
 import static utility.TestConfigurationData.*;
 
 public abstract class BaseTest extends TestSteps {
@@ -27,5 +32,15 @@ public abstract class BaseTest extends TestSteps {
             System.out.println(e);
         }
         requestSpec = builder.build();
+    }
+
+    public String createBoard(String name) {
+        RequestSpecification requestSpecification = given().spec(requestSpec);
+
+        Response response = requestSpecification.queryParam("name", name).
+                contentType(ContentType.JSON).
+                when().post("/1/boards/");
+
+        return(response.then().extract().path("id"));
     }
 }
