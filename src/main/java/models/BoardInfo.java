95d4c738598964a4c1ac60e10dd03dbefbbda5fc
apaluch <apaluch@griddynamics.com>
2022-09-07 05:34:11 +0200

    added annotations to objects

--- src/main/java/models/BoardInfo.java
+++ src/main/java/models/BoardInfo.java
@@ -7,9 +7,14 @@ import lombok.Setter;
 import java.util.ArrayList;
 
 public class BoardInfo {
+    @JsonProperty("LabelNames")
     LabelNames labelNames;
+    @JsonProperty("Limits")
     Limits limits;
+    @JsonProperty("Prefs")
     Prefs prefs;
+    @JsonProperty("Root")
     Root root;
+    @JsonProperty("SwitcherView")
     SwitcherView switcherView;
 }
